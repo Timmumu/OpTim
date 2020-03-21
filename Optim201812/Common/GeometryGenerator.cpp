e7bf0e75a3f1111e4fbc5958e9b4c51329e01214
Timmumu <timhuhk@gmail.com>
2020-03-21 17:00:56 +0800

    by cmd

--- Optim201812/Common/GeometryGenerator.cpp
+++ Optim201812/Common/GeometryGenerator.cpp
@@ -24,7 +24,7 @@ void GeometryGenerator::CreateBox(float width, float height, float depth, MeshDa
 	v[5] = Vertex(+w2, -h2, +d2, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
 	v[6] = Vertex(+w2, +h2, +d2, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
 	v[7] = Vertex(-w2, +h2, +d2, 0.0f, 0.0f, 1.0f, -1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-
+	  
 	// Fill in the top face vertex data.
 	v[8] = Vertex(-w2, +h2, -d2, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
 	v[9] = Vertex(-w2, +h2, +d2, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f);
